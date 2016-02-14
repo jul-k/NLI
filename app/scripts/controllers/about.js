8e8cfca3f8a1581eaa2dc595be5934a5b583e990
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-14 20:11:34 +0200

    Start project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextLevelInterviewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nextLevelInterviewApp
+ */
+var app = angular.module('nextLevelInterviewApp');
+
+app.controller('AboutCtrl', ['$scope', function ($scope) {
+
+}]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextLevelInterviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nextLevelInterviewApp
+ */
+var app = angular.module('nextLevelInterviewApp');
+
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+}]);
