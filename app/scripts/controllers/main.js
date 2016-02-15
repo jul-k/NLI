883c68c2ef537393e9cb4cc3b3a56581b4475762
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-02-15 21:57:51 +0200

    Second section

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,5 +10,38 @@
 var app = angular.module('nextLevelInterviewApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
-
+    $scope.clients = [
+        {
+            name: "Client 1",
+            logo: "client1.jpeg"
+        },
+        {
+            name: "Client 2",
+            logo: "client2.jpg"
+        },
+        {
+            name: "Client 3",
+            logo: "client3.jpeg"
+        },
+        {
+            name: "Client 4",
+            logo: "client4.jpg"
+        },
+        {
+            name: "Client 5",
+            logo: "client5.jpg"
+        },
+        {
+            name: "Client 6",
+            logo: "client6.jpg"
+        },
+        {
+            name: "Client 7",
+            logo: "client7.jpg"
+        },
+        {
+            name: "Client 8",
+            logo: "client8.jpg"
+        }
+    ]
 }]);
